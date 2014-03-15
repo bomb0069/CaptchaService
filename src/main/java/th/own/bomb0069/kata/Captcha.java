d9e801354f1ada6c3f9481f6a00fa538c9311232
karan <bomb0069@gmail.com>
2014-03-16 00:03:39 +0700

    create testCreateCaptchaWith1111ShouldBeReturn1PlusOne for captcha class

--- src/main/java/th/own/bomb0069/kata/Captcha.java
+++ src/main/java/th/own/bomb0069/kata/Captcha.java
@@ -2,4 +2,28 @@ package th.own.bomb0069.kata;
 
 public class Captcha {
 
+	public Captcha (int captchaMode, int leftOperand, int operator, int rightOperand) {
+
+	}
+
+	public String getLeftOperand() {
+		return "1";
+	}
+
+	public String getRightOperand() {
+		return "One";
+	}
+
+	public String getOperator() {
+		return "+";
+	}
+
+	public int getResult() {
+		return 2;
+	}
+
+	public String toString() {
+		return "1 + One";
+	}
+
 }
--- src/test/java/th/own/bomb0069/kata/CaptchaTest.java
+++ src/test/java/th/own/bomb0069/kata/CaptchaTest.java
@@ -1,12 +1,24 @@
 package th.own.bomb0069.kata;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CaptchaTest {
 	@Test
 	public void testCreateNewCaptchaShouldBeReturnCaptchaObject () {
-		Captcha captcha = new Captcha();
+		Captcha captcha = new Captcha(1, 1, 1, 1);
 		assertTrue(captcha instanceof Captcha);
 	}
+
+	@Test
+	public void testCreateCaptchaWith1111ShouldBeReturn1PlusOne () {
+		Captcha captcha = new Captcha(1, 1, 1, 1);
+		assertEquals("1", captcha.getLeftOperand());
+		assertEquals("+", captcha.getOperator());
+		assertEquals("One", captcha.getRightOperand());
+		assertEquals("1 + One", captcha.toString());
+		assertEquals(2, captcha.getResult());
+	}
+
 }
