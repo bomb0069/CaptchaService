bf71326c42bfc92082abcca4ee0b3e4034e41d3c
karan <bomb0069@gmail.com>
2014-03-16 00:31:13 +0700

    add numberText to translate int of operand to text of number

--- src/main/java/th/own/bomb0069/kata/Captcha.java
+++ src/main/java/th/own/bomb0069/kata/Captcha.java
@@ -1,13 +1,33 @@
 package th.own.bomb0069.kata;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Captcha {
 
 	private int leftOperand;
 	private int rightOperand;
+	private int operator;
+
+	private static Map<Integer, String> numberText;
+    static
+    {
+        numberText = new HashMap<Integer, String>();
+        numberText.put(1, "One");
+        numberText.put(2, "Two");
+        numberText.put(3, "Three");
+        numberText.put(4, "Four");
+        numberText.put(5, "Five");
+        numberText.put(6, "Six");
+        numberText.put(7, "Seven");
+        numberText.put(8, "Eight");
+        numberText.put(9, "Nine");
+    }
 
 	public Captcha (int captchaMode, int leftOperand, int operator, int rightOperand) {
 		this.leftOperand = leftOperand;
 		this.rightOperand = rightOperand;
+		this.operator = operator;
 	}
 
 	public String getLeftOperand() {
@@ -15,22 +35,23 @@ public class Captcha {
 	}
 
 	public String getRightOperand() {
-		if (rightOperand == 5)
-			return "Five";
-		else 
-			return "One";
+		return numberText.get(rightOperand);
 	}
 
 	public String getOperator() {
+		if (operator == 2 )
+			return "*";
 		return "+";
 	}
 
 	public int getResult() {
+		if (operator == 2)
+			return leftOperand * rightOperand;
 		return leftOperand + rightOperand;
 	}
 
 	public String toString() {
-		return getLeftOperand() + " + " + getRightOperand();
+		return getLeftOperand() + " " + getOperator() + " " + getRightOperand();
 	}
 
 }
--- src/test/java/th/own/bomb0069/kata/CaptchaTest.java
+++ src/test/java/th/own/bomb0069/kata/CaptchaTest.java
@@ -41,4 +41,33 @@ public class CaptchaTest {
 		assertEquals(8, captcha.getResult());
 	}
 
+	@Test
+	public void testCreateCaptchaWith1325ShouldBeReturn3MultipleFive () {
+		Captcha captcha = new Captcha(1, 3, 2, 5);
+		assertEquals("3", captcha.getLeftOperand());
+		assertEquals("*", captcha.getOperator());
+		assertEquals("Five", captcha.getRightOperand());
+		assertEquals("3 * Five", captcha.toString());
+		assertEquals(15, captcha.getResult());
+	}
+
+	@Test
+	public void testCreateCaptchaWith1822ShouldBeReturn8MultipleTwo () {
+		Captcha captcha = new Captcha(1, 8, 2, 2);
+		assertEquals("8", captcha.getLeftOperand());
+		assertEquals("*", captcha.getOperator());
+		assertEquals("Two", captcha.getRightOperand());
+		assertEquals("8 * Two", captcha.toString());
+		assertEquals(16, captcha.getResult());
+	}
+
+	@Test
+	public void testCreateCaptchaWith1413ShouldBeReturn4PlusThree () {
+		Captcha captcha = new Captcha(1, 4, 1, 3);
+		assertEquals("4", captcha.getLeftOperand());
+		assertEquals("+", captcha.getOperator());
+		assertEquals("Three", captcha.getRightOperand());
+		assertEquals("4 + Three", captcha.toString());
+		assertEquals(7, captcha.getResult());
+	}
 }
